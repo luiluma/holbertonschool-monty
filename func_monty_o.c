e708b13e1f28d00678df8b29c494b50bd8162d56
luiluma <luma24lopez@gmail.com>
2023-05-11 14:30:05 -0500

    change error traslation empty

--- func_monty_o.c
+++ func_monty_o.c
@@ -1,28 +1,5 @@
 #include "monty.h"
 
-int main(void)
-{
-    return (0);
-}
-
-/**
- * is_number - checks if is a number
- * @token: string to check id is a number
- * Return: 0
- */
-int is_number(char *token)
-{
-	int i;
-
-	if (token == NULL)
-		return (-1);
-	for (i = 0; token[i] != '\0'; i++)
-	{
-		if (!(token[i] >= '0' && token[i] <= '9') && token[i] != '-')
-			return (-1);
-	}
-	return (0);
-}
 /**
  * open_file_to_read - Open a monty files
  * @filename: Name if monty file
